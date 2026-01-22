603a7e7844f3689b3709bc4854b7414256680e5e
Joel RR Davis <joelrrdavis@gmail.com>
2026-01-22 09:39:27 -0500

    reorg core generators

--- notebooks/smoke_test.py
+++ notebooks/smoke_test.py
@@ -1,6 +1,6 @@
 # smoke_test.py
 # where there is smoke... 
-from gateway.generators.transaction_risk_dgp import generate_transaction_risk_dataset
+from core.generators.transaction_risk_dgp import generate_transaction_risk_dataset
 
 df = generate_transaction_risk_dataset(seed=1955)
 print(df.shape, df["is_fraud"].mean())
